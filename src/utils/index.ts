fb5c669bbbefc52bf0520653d9f1b343e7f8bce7
BainWang0 <167943073+BainWang0@users.noreply.github.com>
2024-04-26 10:06:12 +0800

    feat: modify jump browser address and text (#4)
    * v0.0.0
    
    * feat: Modify jump browser address and text
    
    * Revert "v0.0.0"
    
    This reverts commit 62c7a7e064c2794f438421c70d076a3896cd7be2.
    
    * feat: update ETHERSCAN_PREFIXES for 168167
    
    ---------
    
    Co-authored-by: bain <xuepeng.w@kaitaitech.cn>
    Co-authored-by: Luke <luchenqun@qq.com>

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -23,11 +23,11 @@ const ETHERSCAN_PREFIXES: { [chainId in ChainId]: string } = {
   4: 'rinkeby.',
   5: 'goerli.',
   42: 'kovan.',
-  168167: 'dev_mud.'
+  168167: 'dev-scan.'
 }
 
 export function getEtherscanLink(chainId: ChainId, data: string, type: 'transaction' | 'token' | 'address'): string {
-  const prefix = `https://${ETHERSCAN_PREFIXES[chainId] || ETHERSCAN_PREFIXES[1]}etherscan.io`
+  const prefix = `https://${ETHERSCAN_PREFIXES[chainId] || ETHERSCAN_PREFIXES[1]}metauserdao.com`
 
   switch (type) {
     case 'transaction': {
