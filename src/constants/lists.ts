c3021e7cdba630fa399c22a9107c8866a8f086b8
Luke <luchenqun@qq.com>
2024-05-29 16:00:57 +0800

    update token list

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,5 +1,5 @@
 // the Uniswap Default token list lives here
 export const DEFAULT_TOKEN_LIST_URL =
-  'https://gist.githubusercontent.com/luchenqun/9e048005a11fe8c7324c6d7e869b2fb0/raw/e0804f3d4402636015c4b0deab736c9dcb28b005/testnet-mud-uniswap-token.json'
+  'https://black-unnecessary-koi-219.mypinata.cloud/ipfs/QmTX3T7eiC41nFRRhChTqaNRrpUSx6Ua5vuX4vjYPC1yNT'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [DEFAULT_TOKEN_LIST_URL]
